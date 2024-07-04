6a2e5bf80b42d2d18e3ed0d919f2232958e83e0a
nhj <nhj1209526@naver.com>
2024-07-04 18:03:59 +0900

    Feat: 글 목록 조회 페이지/기능 구현

--- src/main/java/com/study/domain/post/PostController.java
+++ src/main/java/com/study/domain/post/PostController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class PostController {
@@ -23,10 +25,18 @@ public class PostController {
         return "post/write";
     }
 
-    // 새 게시물 생성
+    // 새 게시글 생성
     @PostMapping("/post/save.do")
     public String savePost(final PostRequest params) {
         postService.savePost(params);
         return "redirect:/post/list.do";
+    }
+
+    // 게시글 목록 페이지
+    @GetMapping("/post/list.do")
+    public String openPostList(Model model) {
+        List<PostResponse> posts = postService.findAllPost();
+        model.addAttribute("posts", posts);
+        return "post/list";
     }
 }
