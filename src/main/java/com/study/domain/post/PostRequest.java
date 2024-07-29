a13c98f2382708736b3d5b403569fb8c04614f25
nhj <nhj1209526@naver.com>
2024-07-29 23:48:34 +0900

    Feat: 파일 수집용 변수 추가

--- src/main/java/com/study/domain/post/PostRequest.java
+++ src/main/java/com/study/domain/post/PostRequest.java
@@ -2,6 +2,10 @@ package com.study.domain.post;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -12,4 +16,5 @@ public class PostRequest {
     private String content;     // 내용
     private String writer;      // 작성자
     private Boolean noticeYn;   // 공지글 여부
+    private List<MultipartFile> files = new ArrayList<>();    // 첨부파일 List
 }
