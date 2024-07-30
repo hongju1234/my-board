bea93e450ef8e77eef3515800a0543148d77ad71
nhj <nhj1209526@naver.com>
2024-07-30 23:54:59 +0900

    Feat: 첨부파일 상세 페이지에 출력 & 추가/변경/삭제

--- src/main/java/com/study/domain/post/PostRequest.java
+++ src/main/java/com/study/domain/post/PostRequest.java
@@ -17,4 +17,5 @@ public class PostRequest {
     private String writer;      // 작성자
     private Boolean noticeYn;   // 공지글 여부
     private List<MultipartFile> files = new ArrayList<>();    // 첨부파일 List
+    private List<Long> removeFileIds = new ArrayList<>(); // 삭제할 첨부파일 id List
 }
